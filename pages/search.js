1774d76b07d392c26a6ff2428d691e82a8823869
komfysach <sach@getkomfy.net>
2022-11-10 14:28:34 +0100

    removed dummy data page used in development to limit Google request quota

--- pages/search.js
+++ pages/search.js
@@ -29,13 +29,12 @@ function Search({results}) {
 export default Search
 
 export async function getServerSideProps(context) {
-  const useDummyData = false
   const startIndex = context.query.start || '0'
 
   const API_KEY = process.env.API_KEY
   const CONTEXT_KEY = process.env.CONTEXT_KEY
 
-  const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json())
+  const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json())
 
   // pass props to frontend
 
